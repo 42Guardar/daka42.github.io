db0bbea1a8149eba66043faa77d6e5bc16700e3d
42Guardar <aveigad69@gmail.com>
2026-06-22 16:40:25 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -290,7 +290,9 @@ export default function App() {
             <div className="flex-1 flex flex-col items-center justify-center relative">
               <div className="absolute -inset-8 bg-pink-300/15 rounded-full blur-2xl pointer-events-none"></div>
               <h2 className="text-slate-800 text-5xl md:text-7xl font-extrabold uppercase tracking-widest select-none relative z-10">
-                ASI QUE....
+                ASI
+                QUE
+                ...
               </h2>
             </div>
 
